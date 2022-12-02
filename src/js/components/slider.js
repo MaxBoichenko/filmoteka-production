45f0d8147a64d6f5c28e72911a4a5a91d146e0ee
DmytroNadolynskyi <113470234+DmytroNadolynskyi@users.noreply.github.com>
2022-12-02 13:12:49 +0200

    slider

---
+++ src/js/components/slider.js
@@ -0,0 +1,33 @@
+import Glide from '@glidejs/glide';
+import filmsCardSliderTpl from '../../templates/card-films-slider.hbs';
+const sliderContainer = document.querySelector('.js-slider-container');
+renderTrendy();
+
+const glide = new Glide('.glide', {
+  type: 'slider',
+  startAt: 0,
+  perView: 7,
+  autoplay: 1000,
+  hoverpause: true,
+  bound: true,
+});
+
+glide.mount();
+
+function renderTrendy() {
+  const url = `https://api.themoviedb.org/3/trending/all/day?api_key=2db21389930b520d8ed134be7fb3bbe4`;
+  return fetch(url)
+    .then(response => response.json())
+    .then(({ results }) => {
+      return results;
+    })
+    .then(renderSliderFilms)
+    .catch(err => {
+      sliderContainer.innerHTML = `<img class="catch-error-pagination" src="${errorUrl}" />`;
+    });
+}
+
+function renderSliderFilms(articles) {
+  sliderContainer.innerHTML = filmsCardSliderTpl(articles);
+
+}
