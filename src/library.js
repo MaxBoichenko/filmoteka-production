a75d16720a8431cf9ae4cad6103073a3e7643718
DmytroNadolynskyi <113470234+DmytroNadolynskyi@users.noreply.github.com>
2022-12-04 23:07:04 +0200

    theme1

--- src/library.js
+++ src/library.js
@@ -3,3 +3,5 @@ import './js/components/modal/modal';
 import './js/components/students-modal';
 import './js/components/localStorage-library';
 import './js/components/go-to-top';
+import './js/components/theme';
+
